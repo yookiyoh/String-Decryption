f7a976fe9184f45320d65dff31478b8e7a4d7818
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-04-21 07:44:36 +0800

    Decrypt the text using the predefined dictionary through the char variable emanated from a for loop

--- String-Decryption.py
+++ String-Decryption.py
@@ -14,3 +14,11 @@ while True:
 
     # Create a dictionary for decryption
     decrypt_dict = {'*': 'a', '&': 'e', '#': 'i', '+': 'o', '!': 'u'}
+
+    # Decrypt the text using the dictionary
+    decrypted_text = ""
+    for char in encrypted_text:
+        if char in decrypt_dict:
+            decrypted_text += decrypt_dict[char]
+        else:
+            decrypted_text += char
