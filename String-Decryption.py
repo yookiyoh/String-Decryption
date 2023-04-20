97b28cd5f6cb9e5e7d777a41671f951470969376
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-04-21 07:07:31 +0800

    pseudocode

--- String-Decryption.py
+++ String-Decryption.py
@@ -1,3 +1,20 @@
 # Ralph Lorenz I. Codilan
 # BSCpE 1-5
 # Object-Oriented Programming - Assignment 2 - #2
+
+# Code Starts
+
+# Input of string for decryption
+
+# Standardized dictionary for string character decryption
+    # Special character "*" signifies "a"
+    # Special character "&" signifies "e"
+    # Special character "#" signifies "i"
+    # Special character "+" signifies "o"
+    # Special character "!" signifies "u"
+
+# Decrypt using the predefined dictionary
+
+# Print the decrypted text
+
+# Ask the user if they want to repeat decryption
